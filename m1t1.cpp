fc9f092686c78930513836bf699c25a07ac4e349
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-01-24 18:49:35 +0000

    finished m1t1

--- m1t1.cpp
+++ m1t1.cpp
@@ -9,5 +9,14 @@ St. Justice
 using namespace std;
 
 int main() {
+    // Greet user
+    cout << "Hello, User!" << endl; // endl = endline
+    cout << "What's your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
+    cout << "Have a good day, " << name << endl;
+    
     return 0;
+
 }
