16b0a803802ecba9e9c1b018c55609ca8584b622
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-01-31 19:25:38 +0000

    Added Tax + Tip + Total, Fixed Tax Rate Number (0.80 > 0.08)

--- M2/m2t2_Youngblood.cpp
+++ M2/m2t2_Youngblood.cpp
@@ -15,7 +15,7 @@ int main () {
     // Declare all variables
     string mealName = "Taco Platter";
     double mealPrice = 7.99;            // $7.99 Price
-    double taxRate = 0.80;              // Tax Rate, 8% == 8/100 Per cent.    
+    double taxRate = 0.08;              // Tax Rate, 8% == 8/100 Per cent.    
     double tipAmount;
     double taxAmount;                   // $ of actual tax charged
     double subTotal;                    // Price before tax/tips
@@ -29,17 +29,26 @@ int main () {
     cout << "Cost for a singular platter is $" << mealPrice << ". How many would you like to purchase? ";
     cin >> num_Meals; 
 
+    // Gather Tip
+    cout << endl << "How much would you like to tip? (Minimum 0) ";
+    cin >> tipAmount; 
 
-    // Do the calculation
+    // Calculate Subtotal
     subTotal = mealPrice * num_Meals; 
+    taxAmount = subTotal * taxRate; // This is ADDED to the bill
+    // Add Tax/Tips
+    totalPrice = subTotal + tipAmount + taxAmount;
     
+
     // Present the output
     cout << endl << "YOUR ORDER: " << endl << "==============" << endl;
     cout << num_Meals << " x " << mealName << "\t$" << mealPrice << endl;
     cout << "Subtotal: \t\t$" << subTotal << endl;
+    cout << "Tip: \t\t\t$" << tipAmount << endl;
+    cout << "Tax: \t\t\t$" << taxAmount << endl;
+    cout << "==============" << endl;
+    cout << "Total: \t\t\t$" << totalPrice << endl;  
 
-    cout << endl << "How much would you like to tip? ";
-    cin >> tipAmount; 
 
     return 0;
 }
