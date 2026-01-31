3102817d25ef0456fbc67e05cd1718bdaa0f8e6c
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-01-31 18:46:42 +0000

    Started M2T2

---
+++ M2/m2t2_Youngblood.cpp
@@ -0,0 +1,17 @@
+/*
+CSC 134
+M2T2 - Recepit calculator
+Youngblood
+1/31/26
+Goal: Create a receipt that handles sales tax
+Assumption: Sales tax is 8% (Varies via County)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+
+    return 0;
+}
