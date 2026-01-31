db310e8ad542203cc2eebad678cc5ec7d2937410
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-01-31 19:00:11 +0000

    Get user input created

--- M2/m2t2_Youngblood.cpp
+++ M2/m2t2_Youngblood.cpp
@@ -19,9 +19,15 @@ int main () {
     double tipAmount;
     double taxAmount;                   // $ of actual tax charged
     double totalPrice;                  // Meal + Meal Tax + Tip
-    
+    int    num_Meals;                   // How many the user purchases
 
     // Get user input
+    cout << "Welcome to the CSC 134 Taco Joint." << endl;
+    cout << "Menu Special for Today: " << mealName << endl;
+    cout << endl;
+    cout << "Cost for a singular platter is $" << mealPrice << ". How many would you like to purchase? ";
+    cin >> num_Meals; 
+    
 
     // Do the calculation
 
