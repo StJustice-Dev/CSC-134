2936b14c1bc0b375e39a0a0bcb53010316e83ac9
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-02-14 19:27:13 +0000

    Started M2Lab

---
+++ M2/m2lab.cpp
@@ -0,0 +1,17 @@
+/*
+CSC 134
+M2LAB1
+Youngblood
+2/14/26
+Goal: Create a program that calculates volume, cost,
+      custromer price, and profit of any crate GCI builds for
+      General Crates, Inc. 
+*/
+
+#include <iostream>
+#include <iomanip> // For 2 decimal places
+using namespace std;
+
+int main() {
+    
+}
