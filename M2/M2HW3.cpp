6600c2f2578ff3fcbb905cf819b410346ddd1380
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-03-01 21:58:26 +0000

    Started M2HW3

---
+++ M2/M2HW3.cpp
@@ -0,0 +1,9 @@
+/*
+CSC 134
+M2HW3
+Youngblood
+3/1/26
+Goal: Create a program that calculates volume, cost,
+      custromer price, and profit of any crate GCI builds for
+      General Crates, Inc. 
+*/
