2e118879693d158529be197c550e8ffd1c6de375
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-03-07 20:48:41 +0000

    Completed M3HW2, Started M3HW3

--- M3/M3HW.cpp
+++ M3/M3HW.cpp
@@ -7,9 +7,13 @@ Goal: Multiple
 */
 
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
 void questionOne();
+void questionTwo();
+void questionThree();
+
 
 int main(){
     
@@ -18,9 +22,16 @@ int main(){
     cout << "Which question would you like to access? (Type 1-3 to access desired question)" << endl;
     cout << "Question 1: Simple Chat Bot (Discussion)" << endl;
     cout << "Question 2: Receipt Calculator (Math)" << endl;
-    cout << "Question 3: Choose Your Own Adventure (Game)" << endl;
+    cout << "Question 3: Music Taste Quiz (Music)" << endl;
     cin >> userQuestion;
 
+    if (userQuestion == 1){
+        questionOne();
+    } else if (userQuestion == 2){
+        questionTwo();
+    } else if (userQuestion == 3){
+        questionThree();
+    }
 
     return 0;
 
@@ -46,7 +57,66 @@ void questionOne(){
 }
 
 void questionTwo(){
+
     // Question 2
     // Goal: Add additional functions to receipt calculator. 
+    
+    // Making Receipt printer for restaurant. 
+    // Declare all variables
+    string mealName = "Taco Platter";
+    const double DINEINTIP = 0.15;      // 15% Dine in Tip
+    double mealPrice = 7.99;            // $7.99 Price
+    double taxRate = 0.08;              // Tax Rate, 8% == 8/100 Per cent. 
+    double dineInFee = 0.00;  
+    double tipAmount;
+    double taxAmount;                   // $ of actual tax charged
+    double subTotal;                    // Price before tax/tips
+    double totalPrice;                  // Subtotal + Meal Tax + Tip
+    int    num_Meals;                   // How many the user purchases
+    int    userDineIn;                  // Determines if the user is dining in or on the go.
+    // Get user input
+    cout << "Welcome to the CSC 134 Taco Joint." << endl;
+    cout << "Menu Special for Today: " << mealName << endl;
+    cout << endl;
+    cout << "Cost for a singular platter is $" << mealPrice << ". How many would you like to purchase?" << endl;
+    cin >> num_Meals; 
 
+    // Verify Dine In and To Go orders. 
+    cout << "Is your order for here or to go? (Type 1 for Dine in orders, Type 2 for To Go Orders.)" << endl;
+    cin >> userDineIn;
+
+    // Gather Tip
+    cout << endl << "How much would you like to tip? (Minimum 0) " << endl;
+    cin >> tipAmount; 
+    // Calculate Subtotal
+    subTotal = mealPrice * num_Meals; 
+    taxAmount = subTotal * taxRate; // This is ADDED to the billsssssssss
+    // Add Tax/Tips
+    totalPrice = subTotal + tipAmount + taxAmount;
+    
+    if (userDineIn == 1){
+        dineInFee = (totalPrice * DINEINTIP);
+        totalPrice += dineInFee;
+    }
+    
+    // Present the output
+    cout << setprecision(2) << fixed; // Set outputs to 2 decimal places
+    cout << endl << "YOUR ORDER: " << endl << "==============" << endl;
+    cout << num_Meals << " x " << mealName << "\t$" << mealPrice << endl;
+    cout << "Subtotal: \t\t$" << subTotal << endl;
+    cout << "Dine In Fee: \t\t$" << dineInFee << endl;
+    cout << "Tip: \t\t\t$" << tipAmount << endl;
+    cout << "Tax: \t\t\t$" << taxAmount << endl;
+    cout << "==============" << endl;
+    cout << "Total: \t\t\t$" << totalPrice << endl;  
+    cout << endl << "THANK YOU, COME AGAIN." << endl;
+
+}
+
+void questionThree(){
+    
+    // Get user to specify music taste
+    int userChoice;
+    
+    
 }
