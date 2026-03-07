1fb33a38dcec03ba06d75ad681854edfcfd880e3
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-03-07 19:03:41 +0000

    Started M3HW Question 2, Reformatted Main

--- M3/M3HW.cpp
+++ M3/M3HW.cpp
@@ -3,14 +3,32 @@ CSC-134
 M3HW
 Youngblood
 3/7/26
-Goal: Create a simple chat bot to have a short discussion.
+Goal: Multiple
 */
 
 #include <iostream>
 using namespace std;
 
-int main(){
+void questionOne();
 
+int main(){
+    
+    // Get user to choose question. 
+    int userQuestion;
+    cout << "Which question would you like to access? (Type 1-3 to access desired question)" << endl;
+    cout << "Question 1: Simple Chat Bot (Discussion)" << endl;
+    cout << "Question 2: Receipt Calculator (Math)" << endl;
+    cout << "Question 3: Choose Your Own Adventure (Game)" << endl;
+    cin >> userQuestion;
+
+
+    return 0;
+
+}
+
+void questionOne(){
+    // Question 1 
+    // Goal: Create a simple chat bot to have a short discussion.
     string userResponse; 
 
     // Start off conversation, prompt user.
@@ -19,12 +37,16 @@ int main(){
     cin >> userResponse;
 
     if (userResponse == "Yes"){
-        cout << "You agree so too! I'm sure we will get along well.";
+        cout << "You agree so too! I'm sure we will get along well." << endl;
     } else if (userResponse == "No"){
-        cout << "Ah, I see. Hope it'll be better tommorow for you!";
+        cout << "Ah, I see. Hope it'll be better tommorow for you!" << endl;
     } else {
-        cout << "Uhh, I didn't quite get that. But it's fine, have a good day!"; 
+        cout << "Uhh, I didn't quite get that. But it's fine, have a good day!" << endl; 
     }
+}
 
-    return 0;
+void questionTwo(){
+    // Question 2
+    // Goal: Add additional functions to receipt calculator. 
+
 }
