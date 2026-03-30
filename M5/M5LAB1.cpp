4f10b5d6fe7868c0c25f6da53287ee3dbc2b6eb6
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-03-30 01:13:18 +0000

    Finished M5LAB1

--- M5/M5LAB1.cpp
+++ M5/M5LAB1.cpp
@@ -3,7 +3,7 @@ CSC 134
 M5LAB1 - Hello, World
 St. Justice
 3/29/26
-This program will ask a question and have the user respond to it.
+Goal: Make a Choose Your Own Adventure game. 
 */
 
 #include <iostream>
@@ -14,54 +14,85 @@ void chooseDoor2();
 void chooseDoor3();
 void chooseFlee();
 
+void main_menu();
+void choice_front_door();
+void choice_back_door();
+void choice_go_home();
+
 int main() {
+  cout << "M5LAB1 - Choose Your Own Adventure" << endl;
+  // load up the main menu
+  main_menu();
+  // when we return here, we're done
+  cout << "Thanks for playing!" << endl;
+  return 0; // finished with no errors
+}
 
-  // Declare variables
-  int choice; 
-
-  // Greeting message, Get user input. 
-  cout << "You come across a monument in your ventures. There appears to be three doors, which one will you enter?" << endl;
-  cout << "Type 1, 2, 3 to go through the desired door. Or enter 4 to leave. Your Number:  "; 
+void main_menu() {
+  // Write a simple menu that lets the user choose 1,2, or 3, or 4 to quit.
+  cout << "Main Menu" << endl;
+  cout << "You're in front of a spooky old house..." << endl;
+  cout << "Do you:" << endl;
+  cout << "1. Try the front door" << endl;
+  cout << "2. Sneak around back" << endl;
+  cout << "3. Forget it, and go home" << endl;
+  cout << "4. [Quit]" << endl;
+  cout << "Choose: ";
+  int choice;
   cin >> choice;
-
-  // Checks for users number and grabs the correct method. 
   if (1 == choice) {
-    chooseDoor1();
+    choice_front_door();
+  } else if (2 == choice) {
+    choice_back_door();
+  } else if (3 == choice) {
+    choice_go_home();
+  } else if (4 == choice) {
+    cout << "Ok, quitting game" << endl;
+    return; // go back to main()
+  } else {
+    cout << "That's not a valid choice, please try again." << endl;
+    cin.ignore(); // clear the user input
+    main_menu();  // try again
   }
-  else if (2 == choice) {
-    chooseDoor2();
-  } 
-  else if (3 == choice) {
-    chooseDoor3();
+}
+
+// FUNCTION DEFINITIONS
+
+void choice_front_door() {
+  cout << "Try the front door." << endl;
+  cout << "It's locked. " << endl;
+  cout << "Do you:" << endl;
+  cout << "1. Check around back" << endl;
+  cout << "2. Give up and go home" << endl;
+  int choice;
+  cout << "Choose: ";
+  cin >> choice;
+  if (1 == choice) {
+    choice_back_door();
+  } else if (2 == choice) {
+    choice_go_home();
   }
-  else if (4 == choice) {
-    chooseFlee();
-  } else { 
-    cout << "I'm sorry, that is not a valid choice." << endl;
-  }
-
-  cout << "Thank you for playing!" << endl;
-  return 0; 
-
-} 
-
-void chooseDoor1() {
-  cout << "You chose Door 1" << endl;
-  cout << "You win ... A Toyota Prius!" << endl;
 }
 
-void chooseDoor2() {
-  cout << "You chose Door 2" << endl;
-  cout << "You win ... Nothing..." << endl;
+void choice_back_door() { 
+    cout << "You sneak around back, coming across a door." << endl;
+    cout << "Your try to lift the handle, but it breaks off." << endl;
+    cout << "Do you:" << endl;
+    cout << "1. Give up and go home" << endl;
+    cout << "2. Give up and go home" << endl;
+    int choice;
+    cout << "Choose: ";
+     cin >> choice;
+    if (1 == choice) {
+        choice_go_home();
+    } else if (2 == choice) {
+        choice_go_home();
+    }
 }
 
-void chooseDoor3() {
-    cout << "You chose Door 3" << endl;
-    cout << "You win ... A bag of apples." << endl;
+void choice_go_home() { 
+    cout << "You decide to go home." << endl;
+    cout << "Adventure can always wait another day." << endl;
 }
 
-void chooseFlee() {
-    cout << "You chose to leave?" << endl;
-    cout << "You win ... A safe venture back home." << endl;
-}
-
+// any new choices go here
