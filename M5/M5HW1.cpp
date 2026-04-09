b45eb73a0ec1a5453e5b9f3692b55293b096174a
StJustice-Dev <youngbls5348@student.faytechcc.edu>
2026-04-09 16:37:34 +0000

    Started M5HW1

---
+++ M5/M5HW1.cpp
@@ -0,0 +1,48 @@
+/*
+CSC 134
+M5HW1
+St. Justice
+4/9/26
+Goal: Solve a multiple of questions. 
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
+
+double averageRainfall(){
+
+    // Declare variables
+    string monthOne, monthTwo, monthThree;
+    double rainfallOne, rainfallTwo, rainfallThree;
+    double average;
+
+    // Get user to input info and variables. 
+    cout << "Enter month 1: ";
+    cin >> monthOne;
+    cout << endl;
+
+    cout << "Enter month 2: ";
+    cin >> monthTwo;
+    cout << endl;
+    
+    cout << "Enter month 3: ";
+    cin >> monthThree;
+    cout << endl;
+
+    // Calculate the average. 
+    average = (monthOne + monthTwo + monthThree) / 3.0;
+
+
+
+}
